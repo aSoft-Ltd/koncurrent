85b7bead1b985b865fba98f7fc641f9c69f9aa9e
andylamax <andylamax@programmer.net>
2023-12-01 13:40:48 +0300

    Added prefix and suffix to useDuration

--- later/coroutines/src/commonMain/kotlin/koncurrent/later/Estimation.kt
+++ later/coroutines/src/commonMain/kotlin/koncurrent/later/Estimation.kt
@@ -17,7 +17,7 @@ class EstimationRate(
  * An estimation function that estimates how much progress will be taken to process a sequence of bytes
  * @param bytes The total number of bytes to be processed
  * @param rate The estimation rate of the process
- * @param max The maximum possible value this estimation can achieve
+ * @param max The maximum possible percentage value this estimation can achieve
  * @param until On which conditions should this estimation terminate
  */
 fun CoroutineScope.estimate(bytes: Int, rate: EstimationRate = EstimationRate(), max: Int = 100, until: () -> Boolean): Later<Unit> = later {
@@ -26,7 +26,7 @@ fun CoroutineScope.estimate(bytes: Int, rate: EstimationRate = EstimationRate(),
     val tt = bytes
     while (!until()) {
         val p = max * (1 - exp(-tx / tt))
-        it.updateProgress(estimating(p.toInt(), 100))
+        it.updateProgress(estimating((p.toInt() * bytes) / 100, bytes))
         delay(rate.duration)
         tx += rate.bytes
     }
