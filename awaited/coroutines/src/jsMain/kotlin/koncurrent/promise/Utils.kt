0e2a6b9955fca321dce3f1bc7d2b7dce6765b0c8
andylamax <andylamax@programmer.net>
2024-08-13 02:21:56 +0300

    Fixed the concurrent import issue

--- awaited/coroutines/src/jsMain/kotlin/koncurrent/promise/Utils.kt
+++ awaited/coroutines/src/jsMain/kotlin/koncurrent/promise/Utils.kt
@@ -3,9 +3,11 @@ package koncurrent.promise
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
+import koncurrent.Promise
 
 
 private inline val Any.isPromise get() : Boolean = asDynamic()?.constructor?.name == "Promise"
+
 suspend fun <T> Promise<T>.await(): T = if (isPromise) suspendCancellableCoroutine { cont ->
     then(
         onFulfilled = { cont.resume(it) },
