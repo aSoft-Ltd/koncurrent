508a62dfc8cb6022ca02524092b9e1159a495d71
andylamax <andylamax@programmer.net>
2025-09-29 15:43:58 +0300

    Bumped version 3.2.3 -> 3.2.4

--- later/core/src/webMain/kotlin/koncurrent/later/JsPromiseUtils.kt
+++ later/core/src/webMain/kotlin/koncurrent/later/JsPromiseUtils.kt
@@ -4,6 +4,7 @@ import koncurrent.Later
 import kotlin.js.JsAny
 import kotlin.js.JsPromiseError
 import kotlin.js.Promise
+import koncurrent.Promise as KPromise
 
 inline fun <T : JsAny> Later<T>.toJsPromise(): Promise<T> = Promise { resolve, reject ->
     then(resolve).catch { reject(it as JsPromiseError) }
@@ -11,5 +12,9 @@ inline fun <T : JsAny> Later<T>.toJsPromise(): Promise<T> = Promise { resolve, r
 
 inline fun <T : JsAny> Promise<T>.asLater(): Later<T> = Later { resolve, reject ->
     then { resolve(it) as JsAny }.catch { reject(Throwable("$it")) as JsAny }
+}
+
+inline fun <T : JsAny> KPromise<T>.asLater(): Later<T> = Later { resolve, reject ->
+    then(onFulfilled = { resolve(it) as JsAny }, onRejected = { reject(Throwable("$it")) as JsAny })
 }
 
